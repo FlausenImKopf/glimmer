bc78f1dab8e4d24fc7f3dac1af71ce097ac5f5f0
Regina Mueller <reginocika.mueller@gmail.com>
2024-05-22 10:49:53 +0200

    issue 4, added HomeView, FreudeTimeline, DankTimeline with Routerlink

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mainview">
+    <RouterView />
+  </div>
+</template>
+
+<style>
+html {
+  display: grid;
+  place-items: center;
+  align-items: normal;
+}
+.mainview {
+  min-height: 100vh;
+  max-width: 20rem;
+  background-color: darkblue;
+  border-radius: 2rem;
+  padding: 1rem;
+}
+</style>
