9283a077319c7c72f795080343d3d02580d7bfa8
FlausenImKopf <gluecksfeder@mailbox.org>
2024-05-22 14:29:16 +0200

    issue 5: added views and a list component

---
+++ src/components/TheList.vue
@@ -0,0 +1,42 @@
+<template>
+  <form id="add-anticipation">
+    <input type="text" :placeholder="placeholderText" />
+    <button>speichern</button>
+  </form>
+  <ul>
+    <li>
+      <form>
+        <label :for="singleNote">
+          <input :id="singleNote" type="checkbox" :name="singleNote" />
+          <span>{{ exampleCheckboxText }}</span>
+          <button>teilen</button>
+        </label>
+      </form>
+    </li>
+  </ul>
+</template>
+
+<!-- hier mit props die Texte erwarten lassen. -->
+<script>
+export default {
+  props: {
+    exampleCheckboxText: {
+      type: String,
+      required: true
+    },
+    singleNote: {
+      type: String,
+      required: true
+    },
+    placeholderText: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import VorfreudenView from '../views/VorfreudenView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/vorfreuden',
+      name: 'Vorfreuden',
+      component: VorfreudenView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/dankbarkeiten',
+      name: 'dankbarkeiten',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/DankbarkeitsView.vue')
     }
   ]
 })
