5c5dd4b2529b18d5401069afaa1828a1758c89f1
Regina Mueller <reginocika.mueller@gmail.com>
2024-05-22 21:40:18 +0200

    fix rebase branch

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
       path: '/vorfreuden',
       name: 'Vorfreuden',
       component: VorfreudenView
@@ -19,14 +24,15 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/DankbarkeitsView.vue')
-      
+    },
+    {
       path: '/danktimeline',
       name: 'dank',
       component: DankTimeLineView
     },
     {
       path: '/freudetimeline',
-      name: 'fruede',
+      name: 'freude',
       component: FreudeTimeLineView
     }
   ]
