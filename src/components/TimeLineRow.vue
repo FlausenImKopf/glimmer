86e3fa74a8a1a0e5045660e100a8db022444114c
Regina Mueller <reginocika.mueller@gmail.com>
2024-05-24 10:26:32 +0200

    issue 17 added sorted view

---
+++ src/components/TimeLineRow.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>{{ note.message }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    note: Object
+  }
+}
+</script>
+
+<style scoped>
+p {
+  margin: 0;
+}
+</style>
