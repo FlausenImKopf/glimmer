2036f43e65cf5c2254be4327a260d3a8df391d10
Regina Mueller <reginocika.mueller@gmail.com>
2024-05-26 12:48:05 +0200

    issue 28 added calendar in TimeLine

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VCalendar from 'v-calendar'
+import 'v-calendar/style.css'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+// Use plugin with optional defaults
+app.use(VCalendar, {})
 
 app.mount('#app')
